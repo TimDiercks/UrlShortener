a0efff4462fce791c877c67bf306738f294813d6
Tim Diercks <tim.r.diercks@gmail.com>
2022-12-12 22:43:22 +0100

    core: added database structure

---
+++ server/schema/postgres.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS user (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    name text,
+    email text NOT NULL,
+    password bytea NOT NULL,
+    salt bytea NOT NULL,
+    created_at timestamp NOT NULL DEFAULT now(),
+    updated_at timestamp NOT NULL DEFAULT now(),
+    UNIQUE(email)
+);
+
+CREATE TABLE IF NOT EXISTS urls (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id uuid NOT NULL REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    shortened text NOT NULL,
+    full text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS apikeys (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id uuid NOT NULL REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    key text NOT NULL
+);
