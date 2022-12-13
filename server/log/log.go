480ff55a9e07341cce8806d3b655302d5aebcb84
Tim Diercks <tim.r.diercks@gmail.com>
2022-12-13 17:23:55 +0100

    added logger

---
+++ server/log/log.go
@@ -0,0 +1,39 @@
+package log
+
+import (
+	"log"
+	"os"
+)
+
+type Logger struct {
+	infoLogger    *log.Logger
+	warningLogger *log.Logger
+	errorLogger   *log.Logger
+}
+
+func New() Logger {
+	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		panic("Error: Log file could not be created")
+	}
+
+	var logger Logger
+
+	logger.infoLogger = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	logger.warningLogger = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	logger.errorLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	return logger
+}
+
+func (logger *Logger) Info(info string) {
+	logger.infoLogger.Println(info)
+}
+
+func (logger *Logger) Warning(warning string) {
+	logger.warningLogger.Println(warning)
+}
+
+func (logger *Logger) Error(err string) {
+	logger.errorLogger.Println(err)
+}
