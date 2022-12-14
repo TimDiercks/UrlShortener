88c8415d9eaca7786e856d80840f443d5f7b0bc9
Tim Diercks <tim.diercks@tuhh.de>
2022-12-14 08:38:23 +0100

    update sql schema

--- server/schema/postgres.sql
+++ server/schema/postgres.sql
@@ -12,13 +12,17 @@ CREATE TABLE IF NOT EXISTS urls (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id uuid,
     short_url text NOT NULL,
-    full_url text NOT NULL
+    full_url text NOT NULL,
+    created_at timestamp NOT NULL DEFAULT now(),
+    updated_at timestamp NOT NULL DEFAULT now(),
     UNIQUE(short_url)
 );
 
 CREATE TABLE IF NOT EXISTS apikeys (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id uuid NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    key text NOT NULL
+    key text NOT NULL,
+    created_at timestamp NOT NULL DEFAULT now(),
+    updated_at timestamp NOT NULL DEFAULT now(),
     UNIQUE(user_id)
 );
