0a97d1c6212287202ba3c89a188853eac87f8f8d
Tim Diercks <tim.r.diercks@gmail.com>
2022-12-20 22:20:16 +0100

    fix cors

--- server/api/api.go
+++ server/api/api.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"net/http"
 	"urlshortener/app"
 	"urlshortener/auth"
 
@@ -23,10 +24,24 @@ func (api *Api) InitRoutes(router *mux.Router) {
 
 	apiPrefix := "/api/v1/"
 
-	router.Handle(apiPrefix, api.auth.KeyAuth(api.AddNewShort)).Methods("POST")
-	router.Handle(apiPrefix, api.auth.KeyAuth(api.GetUrlsForAccessToken)).Methods("GET")
-	router.Handle(apiPrefix+"{shorturl}", api.auth.KeyAuth(api.GetFullFromShort)).Methods("GET")
+	router.Handle(apiPrefix, corsMiddleware(api.auth.KeyAuth(api.AddNewShort))).Methods("POST", "OPTIONS")
+	router.Handle(apiPrefix, corsMiddleware(api.auth.KeyAuth(api.GetUrlsForAccessToken))).Methods("GET", "OPTIONS")
+	router.Handle(apiPrefix+"{shorturl}", corsMiddleware(api.auth.KeyAuth(api.GetFullFromShort))).Methods("GET", "OPTIONS")
 
-	router.HandleFunc("/{shorturl}", api.Redirect).Methods("GET")
+	router.HandleFunc("/{shorturl}", api.Redirect).Methods("GET", "OPTIONS")
+}
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
+		w.Header().Add("Access-Control-Allow-Credentials", "true")
+		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("content-type", "application/json;charset=UTF-8")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
